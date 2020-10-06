ff46257e4e9efa5837a2416b52bb9e59df7474be
Duncanwar <semunda9@gmail.com>
2020-10-06 11:05:43 +0200

    feat(create):Create an article -Created an article with firebase [Finishes (new article)]

---
+++ UI/assets/js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAxn4WuYdT_jXBxT893SgdXuv6m56t_jL0",
+    authDomain: "fir-test-83ee9.firebaseapp.com",
+    databaseURL: "https://fir-test-83ee9.firebaseio.com",
+    projectId: "fir-test-83ee9",
+    storageBucket: "fir-test-83ee9.appspot.com",
+    messagingSenderId: "424866087548",
+    appId: "1:424866087548:web:486d79c81bbb500b1c7ff9",
+    measurementId: "G-7C7L4VCDNS"
+  };
+  firebase.initializeApp(firebaseConfig);
--- UI/assets/js/queries.js
+++ UI/assets/js/queries.js
@@ -1,17 +1,3 @@
-// Contact Form 
-var firebaseConfig = {
-    apiKey: "AIzaSyAxn4WuYdT_jXBxT893SgdXuv6m56t_jL0",
-    authDomain: "fir-test-83ee9.firebaseapp.com",
-    databaseURL: "https://fir-test-83ee9.firebaseio.com",
-    projectId: "fir-test-83ee9",
-    storageBucket: "fir-test-83ee9.appspot.com",
-    messagingSenderId: "424866087548",
-    appId: "1:424866087548:web:486d79c81bbb500b1c7ff9",
-    measurementId: "G-7C7L4VCDNS"
-  };
-  firebase.initializeApp(firebaseConfig);
-
-  
 // Declarations
 const queryRef = firebase.database().ref('queries')
 const queryTable = document.getElementById('blog')
