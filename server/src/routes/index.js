3f7185d122191f7138f23664000b7da6a4557c3d
Duncanwar <semunda9@gmail.com>
2020-10-12 18:52:58 +0200

    chore(routes):Set up project routes - Implemented article route - Implemented sigin/signup routes - Implemented query routes - Implemented updateUserInfo route [Finishes (article,query,user,sigin/signup)]

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import authRoutes from './auth.routes';
 import articleRoutes from './article.routes';
 import queriesRoutes from './queries.routes';
+import userInfoRoutes from './user.routes';
 
 dotenv.config();
 
@@ -13,5 +14,6 @@ const baseUrl = `/api/${apiVersion}`;
 router.use(baseUrl, authRoutes);
 router.use(baseUrl, articleRoutes);
 router.use(baseUrl, queriesRoutes);
+router.use(baseUrl, userInfoRoutes);
 
 export default router;
